94c07ee22d912e685487b50da5bd39965baf4f18
Hernan Y.Ke <yuriqiao@gmail.com>
2018-02-25 18:54:49 -0500

    Update JumpGame.java

--- 01/JumpGame.java
+++ 01/JumpGame.java
@@ -1,3 +1,28 @@
 //from 0...n-1
 //n-1-i<=ai
 //f[j]=OR  0<=i<j  (f[i] AND i+a[i]>=j)
+public class JumpGame {
+    /*
+     * @param A: A list of integers
+     * @return: A boolean
+     */
+    public boolean canJump(int[] A) {
+        // write your code here
+        if(A==null||A.length==0){
+            return false;
+        }
+        
+        int n=A.length;
+        boolean[] f = new boolean[n];
+        f[0]=true;
+        for(int j =1;j<n;++j){
+            for(int i=0;i<j;i++){
+                if(f[i]&&i+A[i]>=j){
+                    f[j]=true;
+                    break;
+                }
+            }
+        }
+        return f[n-1];
+    }
+}
